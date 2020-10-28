1c800be86823461affd1b2232accf3e666028e50
pufupufu <55405796+pufupufu@users.noreply.github.com>
2020-10-28 16:29:34 -0300

    D28M10A20H1630

--- Desarrollo de Algoritmos/Recursividad/tpRecursividad/ejercicio5.java
+++ Desarrollo de Algoritmos/Recursividad/tpRecursividad/ejercicio5.java
@@ -5,21 +5,27 @@ import java.util.Scanner;
 public class ejercicio5 {
     public static void main(String[] args) {
         Scanner pufu = new Scanner(System.in);
-        int num;
+        int num, aux, i = 1;
 
         System.out.print("Ingrese un numero: ");
         num = pufu.nextInt();
+        aux = num;
 
-        System.out.println(invertirNumero(num));
+        while (aux >= 10) {
+            i *= 10;
+            aux /= 10;
+        }
+
+        System.out.println(invertirNumero(num, i));
     }
 
-    public static String invertirNumero(int n) {
-        String retorno = "";
+    public static int invertirNumero(int n, int i) {
+        int retorno = 0;
 
         if (n >= 10) {
-            retorno += n % 10 + invertirNumero(n / 10);
+            retorno += ((n % 10) * i) + invertirNumero(n / 10, i / 10);
         } else {
-            retorno += n;
+            retorno = n;
         }
         return retorno;
     }
